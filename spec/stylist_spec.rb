949066061ac4fd987ba4738a05bbe0a4479a64dc
Stephanie Gurung <stephr3@gmail.com>
2016-07-15 08:44:15 -0700

    pass specs Stylist#phone and Stylist#specialty

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,6 +8,20 @@ describe(Stylist) do
     end
   end
 
+  describe("#phone") do
+    it("returns the stylist's phone number") do
+      stylist = Stylist.new({:name => "Susan Sontag", :phone => "360-134-7483", :specialty => "color", :id => nil})
+      expect(stylist.phone()).to(eq("360-134-7483"))
+    end
+  end
+
+  describe("#specialty") do
+    it("returns the stylist's specialty") do
+      stylist = Stylist.new({:name => "Susan Sontag", :phone => "360-134-7483", :specialty => "color", :id => nil})
+      expect(stylist.specialty()).to(eq("color"))
+    end
+  end
+
   describe(".all") do
     it("initializes with an empty array") do
       expect(Stylist.all()).to(eq([]))
