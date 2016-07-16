2a8427e407b8b33adfedcffb8458a9200083eee2
Stephanie Gurung <stephr3@gmail.com>
2016-07-15 19:23:07 -0700

    Change database required for app.rb file

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require("./lib/client")
 require("./lib/stylist")
 require("pg")
 
-DB = PG.connect({:dbname => 'hair_salon_test'})
+DB = PG.connect({:dbname => 'hair_salon'})
 
 get('/') do
   erb(:index)
